bc92e31f2152597ca943cbbd3005a4da2515d5c9
Adam M Duff <fikosan@gmail.com>
2018-07-26 16:37:08 -0400

    bug: removed problematic return statement

--- js/main.js
+++ js/main.js
@@ -8,8 +8,6 @@ var markers = []
  * Register Service Worker
  */
 
-if (!navigator.serviceWorker) return;
-
 navigator.serviceWorker.register('/sw.js').then(function(reg) {
   console.log("Registration worked!");
 });
