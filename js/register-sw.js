570c10c26d5dee397a27e1e028bc00fb692647fa
Adam M. Duff <fikosan@gmail.com>
2018-09-10 19:36:55 -0400

    bug: Fixing issue with sw online

--- js/register-sw.js
+++ js/register-sw.js
@@ -1,5 +1,9 @@
+let swUrl;
+
+location.hostname === 'fikoreborn.github.io' ? swUrl = '/mws-restaurant-stage-1/sw.js' : swUrl = '/sw.js';
+
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('sw.js').then(function (reg) {
+  navigator.serviceWorker.register(swUrl).then(function (reg) {
     console.log("Registration worked!");
   });
 }
